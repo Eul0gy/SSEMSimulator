9665afe2e79a86e22fd4ed69dfc6c91305534b6f
Eul0gy <7442787+Eul0gy@users.noreply.github.com>
2018-03-12 11:17:50 +0000

    wibble

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace test
            
          SSEM ssem = new SSEM();
          ssem.Menu();
-         Console.WriteLine("Testing visual studio code andd Github ntergraion");
+         Console.WriteLine("wibble"");
       
                 
             
